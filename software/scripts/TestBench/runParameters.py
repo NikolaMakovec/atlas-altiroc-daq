19de92b256d0f4949e967565c53a416fb46b5116
sabrinas <sabrina.sacerdoti@cern.ch>
2020-12-16 08:04:05 +0100

    B28

--- software/scripts/TestBench/runParameters.py
+++ software/scripts/TestBench/runParameters.py
@@ -1,5 +1,5 @@
 doThres     = 0
-doNoise     = 0 # Thres with high stat for few Q
+doNoise     = 1 # Thres with high stat for few Q
 doLinearity = 0 # Thres for many Q
 
 doTW        = 0
@@ -8,7 +8,7 @@ doTWscan    = 0
 doPS        = 0 # TW with thres. scan
 doPSdelay   = 0 # TW with thres. scan
 
-doTOA       = 1
+doTOA       = 0
 doClockTree = 0 # TOA with at least Q=63 and maybe larger N
 doDNL       = 0 # TOA step=1
 doXtalk     = 0 # TOA Channels should be ON
@@ -18,8 +18,9 @@ doVthcScan=0
 #ch list
 chList=None
 #chList=[3]#,3,6,10]#B29
+#chList=[3,6,10,19]#B28
 #chList=[1,3,7,15,17]#B13
-#chList=[0]
+#chList=[1]
 #chList=range(0,25)
 
 #cd list 
@@ -28,13 +29,13 @@ cdList=[4,7]
 
 #TOA
 doFullQScanForTOA = 0
-#QTOAList=[7,13,26]#v2
-#QTOAList=[6,9,16]#v3
+#QTOAList=[7,13,26,63]#v2
+QTOAList=[6,9,16,32,63]#v3
 #QTOAList=[7]  #v2:5,7,13  v3:6,9,16
 
 #THRES
 QThresList=[3,4]#default
-QThresList=[4]
+QThresList=[4,5]
 
 
 
