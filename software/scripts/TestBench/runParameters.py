82b4748aacffae66ab2a544960b3fbd714b96fe0
Sabrina Sacerdoti <sabrina.sacerdoti@cern.ch>
2020-12-17 09:49:08 +0100

    add old TB config

--- software/scripts/TestBench/runParameters.py
+++ software/scripts/TestBench/runParameters.py
@@ -1,12 +1,12 @@
 doThres     = 0
-doNoise     = 1 # Thres with high stat for few Q
+doNoise     = 0 # Thres with high stat for few Q
 doLinearity = 0 # Thres for many Q
 
 doTW        = 0
 doTWdelay   = 0
 doTWscan    = 0
 doPS        = 0 # TW with thres. scan
-doPSdelay   = 0 # TW with thres. scan
+doPSdelay   = 1 # TW with thres. scan
 
 doTOA       = 0
 doClockTree = 0 # TOA with at least Q=63 and maybe larger N
@@ -19,7 +19,7 @@ doVthcScan=0
 chList=None
 #chList=[3]#,3,6,10]#B29
 #chList=[3,6,10,19]#B28
-#chList=[1,3,7,15,17]#B13
+chList=[3,12,13,15]#,17,21]#B13
 #chList=[1]
 #chList=range(0,25)
 
@@ -29,8 +29,8 @@ cdList=[4,7]
 
 #TOA
 doFullQScanForTOA = 0
-#QTOAList=[7,13,26,63]#v2
-QTOAList=[6,9,16,32,63]#v3
+QTOAList=[13,26,63]#v2:7,13,26,63
+#QTOAList=[6,9,16,32,63]#v3
 #QTOAList=[7]  #v2:5,7,13  v3:6,9,16
 
 #THRES
